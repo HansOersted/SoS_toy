275c31450911a0a39ca68e17da53d0e6ac9aeb7f
HansOersted <lucidshenzhe@gmail.com>
2025-07-02 12:27:54 +0200

    include all the terms

--- SoS_SDP.m
+++ SoS_SDP.m
@@ -12,7 +12,7 @@ f = [-x1 + x2^3;
 prog = sosprogram(vars);
 
 % Lyapunov is defined second-order
-monom = monomials(vars, 2);  % monomial basis（x1^2, x1*x2, x2^2）
+monom = monomials(vars, 0:2);  % monomial basis e.g., 2 --> x1^2, x1*x2, x2^2
 [prog, V] = sospolyvar(prog, monom, 'wscoeff');
 
 % V(x) > ε‖x‖²
@@ -22,7 +22,7 @@ prog = sosineq(prog, V - eps*(x1^2 + x2^2));  % guarantee V > 0 and V([0,0]) == 
 % calculate the derivative of V
 Vdot = jacobian(V, vars) * f;
 
-% negative definite guarantee：-Vdot > 0, Vdot < 0
+% negative definite guarantee: -Vdot > 0, Vdot < 0
 prog = sosineq(prog, -Vdot);
 
 % solve
